c0e1de19e24dc5ca1ef1f4b0034df2ed000d6490
Lu Shang Kun <leonlu178@gmail.com>
2023-05-23 11:28:00 +0800

    update App.js
    Signed-off-by: Lu Shang Kun <leonlu178@gmail.com>

--- app/App.js
+++ app/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import LoginPage from './pages/LoginPage';
 import StackNavigator from './StackNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View>
+    <NavigationContainer>
       <LoginPage>
         <StackNavigator/>
       </LoginPage>
-    </View>
+    </NavigationContainer>
   );
 };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
