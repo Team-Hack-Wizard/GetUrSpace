1803b023bdd00e2c50e3be6835981b16e745653e
leonlu178 <leonlu178@gmail.com>
2023-05-25 16:07:18 +0800

    update pages
    Signed-off-by: leonlu178 <leonlu178@gmail.com>

---
+++ app/AuthNavigator.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ForgotPWPage from './pages/ForgotPWPage';
+
+export default function AuthNavigator() {
+    const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' component={LoginPage} />
+        <Stack.Screen name='Register' component={RegisterPage} />
+        <Stack.Screen name='Forgot Password' component={ForgotPWPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({})
--- app/pages/FacilitiesPage.js
+++ app/pages/FacilitiesPage.js
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React, { useLayoutEffect, useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
-const FacilitiesPage = () => {
+export default function FacilitiesPage() {
+    const navigation = useNavigation();
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearch = () => {
+        console.log('Performing search: ', searchQuery);
+    }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+        headerShown:true,
+        headerTitleStyle: {
+            fontSize: 20,
+            fontWeight: "bold",
+        }
+    })
+  },[])
+
   return (
     <View>
-      <Text>FacilitiesPage</Text>
+      <TextInput
+        style={styles.search}
+        onChangeText={text => setSearchQuery(text)}
+        value={searchQuery}
+        placeholder='Search'
+      />
     </View>
   )
 }
 
-export default FacilitiesPage
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  search: {
+    width: "70%",
+    borderRadius: 30,
+    height: 45,
+    backgroundColor: 'gray',
+  },
+})
